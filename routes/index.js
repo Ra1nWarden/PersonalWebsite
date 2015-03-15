2cb1f7a9b14510ddc7f634e7fa9c060ff2f0e39e
Ra1nWarden <wzh19921016@gmail.com>
2015-03-15 13:37:38 -0400

    Added some homepage

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Alan Wang" });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/blog', function(req, res, next) {
+  res.redirect('https://wzh19921016.wordpress.com/');
+});
+
+router.get('/github', function(req,res, next) {
+  res.redirect('https://github.com/Ra1nWarden');
+});
+
 module.exports = router;
