23cbadfa50e0ec4901d934b4b5e9a3bc5e997932
Ra1nWarden <wzh19921016@gmail.com>
2015-03-31 02:00:41 -0400

    Added some more pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PersonalWebsite
+My [personal website](http://ra1nwarden.me) written in node.js and hosted by Heroku.
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,16 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
+router.get('/algorithm', function(req, res, next) {
+  res.render('algorithm');
+});
+
+router.get('/hobbies', function(req, res, next) {
+  res.render('hobbies');
+});
+
+router.get('/messages', function(req, res, next) {
+  res.render('messages');
+});
+
 module.exports = router;
